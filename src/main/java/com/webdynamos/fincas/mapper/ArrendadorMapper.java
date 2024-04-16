c8848d9815f154e84df7ef7f23c702a0470b2fcc
Eddie918 <arix918@gmail.com>
2024-04-16 18:04:24 -0500

    cambios

--- src/main/java/com/webdynamos/fincas/mapper/ArrendadorMapper.java
+++ src/main/java/com/webdynamos/fincas/mapper/ArrendadorMapper.java
@@ -1,16 +1,16 @@
 package com.webdynamos.fincas.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import com.webdynamos.fincas.dto.ArrendadorDTO;
 import com.webdynamos.fincas.models.Arrendador;
+import org.mapstruct.Mapping; // Add this import statement
 
 @Mapper(componentModel = "spring")
 public interface ArrendadorMapper {
     
-    @Mapping(source = "id_arrendador", target = "id")
+    @Mapping(source = "idArrendador", target = "idArrendador") // Update the target property name to "idArrendador"
     @Mapping(source = "nombre", target = "nombre")
     @Mapping(source = "apellido", target = "apellido")
-    @Mapping(source = "correo", target = "correo")
+    @Mapping(source = "correo", target = "email") // Update the target property name to "email"
     ArrendadorDTO arrendadorToArrendadorDTO(Arrendador arrendador);
 }
