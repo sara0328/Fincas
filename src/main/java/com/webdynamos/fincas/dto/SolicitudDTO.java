638a5e159ae788ce0055e0cbd6fd81fdb3c3c2f2
Eddie918 <arix918@gmail.com>
2024-04-23 15:33:17 -0500

    cambios

--- src/main/java/com/webdynamos/fincas/dto/SolicitudDTO.java
+++ src/main/java/com/webdynamos/fincas/dto/SolicitudDTO.java
@@ -9,4 +9,7 @@ public class SolicitudDTO {
     private Long id_solicitud;
     private int duracion;
     private int estado;
+    private Long id_arrendador;
+    private Long id_arrendatario;
+    private Long id_propiedad;
 }
