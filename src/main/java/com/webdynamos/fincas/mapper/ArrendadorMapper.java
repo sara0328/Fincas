9ebbbe6b66dff4c5b3f0e3d2e2b89ca1baa9748e
Jerson Osorio <61725276+Velorio@users.noreply.github.com>
2024-04-25 10:24:19 -0500

    arreglado problema de mapper

--- src/main/java/com/webdynamos/fincas/mapper/ArrendadorMapper.java
+++ src/main/java/com/webdynamos/fincas/mapper/ArrendadorMapper.java
@@ -8,13 +8,9 @@ import org.mapstruct.Mapping; // Add this import statement
 @Mapper(componentModel = "spring")
 public interface ArrendadorMapper {
 
-    @Mapping(source = "id_arrendador", target = "id_arrendador")
-    @Mapping(source = "nombre", target = "nombre")
-    @Mapping(source = "apellido", target = "apellido")
-    @Mapping(source = "correo", target = "correo")
-    @Mapping(source = "telefono", target = "telefono")
-    @Mapping(source = "calificacion", target = "calificacion")
     ArrendadorDTO arrendadorToArrendadorDTO(Arrendador arrendador);
+
+    Arrendador arrendadorDTOToArrendador(ArrendadorDTO arrendadorDTO);
 }
 
 
