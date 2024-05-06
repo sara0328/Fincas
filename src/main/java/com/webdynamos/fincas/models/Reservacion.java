8cb94a13ce611e3656eb56bdd41e2e016b36a223
Eddie918 <arix918@gmail.com>
2024-05-06 17:13:43 -0500

    getters, setters Reservacion

--- src/main/java/com/webdynamos/fincas/models/Reservacion.java
+++ src/main/java/com/webdynamos/fincas/models/Reservacion.java
@@ -23,6 +23,49 @@ public class Reservacion {
     private LocalDateTime fechaExpiracion;
     private String estado;
     private Double deposito;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Propiedad getPropiedad() {
+        return propiedad;
+    }
+    public void setPropiedad(Propiedad propiedad) {
+        this.propiedad = propiedad;
+    }
+    public Usuario getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    public LocalDateTime getFechaReserva() {
+        return fechaReserva;
+    }
+    public void setFechaReserva(LocalDateTime fechaReserva) {
+        this.fechaReserva = fechaReserva;
+    }
+    public LocalDateTime getFechaExpiracion() {
+        return fechaExpiracion;
+    }
+    public void setFechaExpiracion(LocalDateTime fechaExpiracion) {
+        this.fechaExpiracion = fechaExpiracion;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public Double getDeposito() {
+        return deposito;
+    }
+    public void setDeposito(Double deposito) {
+        this.deposito = deposito;
+    }
 
     // Getters and setters
+    
 }
