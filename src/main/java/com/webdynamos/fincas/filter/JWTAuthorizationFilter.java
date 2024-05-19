1ef45bc1bc2c46913f921a77b52530136df8f998
sara0328 <saritasuarez8028@gmail.com>
2024-05-19 14:41:20 -0500

    Update JWTAuthorizationFilter.java

--- src/main/java/com/webdynamos/fincas/filter/JWTAuthorizationFilter.java
+++ src/main/java/com/webdynamos/fincas/filter/JWTAuthorizationFilter.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.webdynamos.fincas.services.CustomUserDetailsService;
@@ -24,38 +23,29 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
 @Component
-@Service
-public class JWTAuthorizationFilter extends OncePerRequestFilter{
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
-
-    public static final String HEADER = "Authorization";
+	public static final String HEADER = "Authorization";
 	public static final String PREFIX = "Bearer ";
 
-    @Autowired
+	@Autowired
 	private CustomUserDetailsService userDetailsService;
 
-    @Autowired
-    private JWTTokenService jwtTokenService;
-
-	
+	@Autowired
+	private JWTTokenService jwtTokenService;
 
 	@Override
-	protected void doFilterInternal( @NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain chain) throws ServletException, IOException {
-		System.out.println( "-------->>>-------->>> Filtro"  );
-		System.out.println( "-------->>>-------->>> Filtro"  );
-		System.out.println( "-------->>>-------->>> Filtro"  );
-		System.out.println( "-------->>>-------->>> Filtro"  );
+	protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain chain) throws ServletException, IOException {
 		try {
 			if (existeJWTToken(request)) {
 				Claims claims = validarToken(request);
 				if (claims.get("authorities") != null) {
 					String username = getUsername(request);
 					UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-					UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, userDetails, null);
+					UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 					auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-           			SecurityContextHolder.getContext().setAuthentication(auth);
+					SecurityContextHolder.getContext().setAuthentication(auth);
 				} else {
 					SecurityContextHolder.clearContext();
 				}
@@ -66,18 +56,20 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter{
 		} catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException e) {
 			response.sendError(HttpServletResponse.SC_FORBIDDEN, e.getMessage());
 		}
-	}	
+	}
 
 	private Claims validarToken(HttpServletRequest request) {
 		String jwtToken = request.getHeader(HEADER).replace(PREFIX, "");
 		return jwtTokenService.decodeToken(jwtToken);
 	}
+
 	private String getUsername(HttpServletRequest request) {
 		String jwtToken = request.getHeader(HEADER).replace(PREFIX, "");
 		return jwtTokenService.getUsername(jwtToken);
 	}
+
 	private boolean existeJWTToken(HttpServletRequest request) {
 		String authenticationHeader = request.getHeader(HEADER);
-		return !(authenticationHeader == null || !authenticationHeader.startsWith(PREFIX));
+		return authenticationHeader != null && authenticationHeader.startsWith(PREFIX);
 	}
 }
